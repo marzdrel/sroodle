9fa578108df91d40931cd7ffce0125c3714cb185
Marz Drel <marzdrel@users.noreply.github.com>
2025-05-18 22:21:50 +0200

    Add Validation logic

--- app/controllers/polls_controller/create_facade.rb
+++ app/controllers/polls_controller/create_facade.rb
@@ -13,14 +13,36 @@ class PollsController
     end
 
     def call
-      errors = Poll.create(name: params[:name]).errors.to_hash
-      errors = errors.transform_values(&:to_sentence)
+      poll.valid?
 
-      Result.new(false, {}, errors)
+      Result.new(
+        errors: errors,
+        success?: poll.valid?,
+        data: {},
+      )
     end
 
     private
 
     attr_accessor :params
+
+    def errors
+      poll.errors.to_hash.transform_values(&:to_sentence)
+    end
+
+    def poll
+      @_poll ||= Poll.new(poll_strong_params)
+    end
+
+    def poll_strong_params
+      {
+        name: params.fetch(:event),
+        description: params.fetch(:description),
+      }
+    end
+
+    def strong_params
+      # params.require(:poll).expect(:name, :email, :description)
+    end
   end
 end
