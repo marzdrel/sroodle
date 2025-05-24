64001243a0dfc8a48aba069a807a58deb29c3296
Marz Drel <marzdrel@users.noreply.github.com>
2025-05-24 17:35:35 +0200

    Fix style issues

--- app/controllers/votes_controller.rb
+++ app/controllers/votes_controller.rb
@@ -3,16 +3,22 @@ class VotesController < ApplicationController
     result = CreateFacade.call(params)
 
     if result.success?
-      render inertia: "Poll/Show", props: {
-        poll: result.data[:poll],
-        flash: {notice: "Vote was successfully created."}
-      }
+      render(
+        inertia: "Poll/Show",
+        props: {
+          poll: result.data[:poll],
+          flash: { notice: "Vote was successfully created." },
+        },
+      )
     else
-      render inertia: "Poll/Show", props: {
-        poll: result.data[:poll],
-        errors: result.errors,
-        flash: {alert: "Unable to create vote."}
-      }
+      render(
+        inertia: "Poll/Show",
+        props: {
+          poll: result.data[:poll],
+          errors: result.errors,
+          flash: { alert: "Unable to create vote." },
+        },
+      )
     end
   end
 end
--- test/models/poll_test.rb
+++ test/models/poll_test.rb
@@ -8,24 +8,29 @@ class PollTest < ActiveSupport::TestCase
 
   test "belongs to creator" do
     assert_must belong_to(:creator), Poll.new
+    assert_must have_many(:options).dependent(:destroy), Poll.new
+    assert_must have_many(:votes).dependent(:destroy), Poll.new
   end
 
   test "requires a name" do
     assert_must validate_presence_of(:name), Poll.new
-  end
 
-  test "validates name length" do
-    assert_must validate_length_of(:name).is_at_least(5).is_at_most(100), Poll.new
+    assert_must(
+      validate_length_of(:name).is_at_least(5).is_at_most(100),
+      Poll.new,
+    )
   end
 
   test "has exid functionality" do
     poll = polls(:one)
+
     assert_not_nil poll.eid
     assert_equal poll.exid_value, poll.to_param
   end
 
   test "creator association works" do
     poll = polls(:one)
+
     assert_equal users(:alice), poll.creator
   end
 end
