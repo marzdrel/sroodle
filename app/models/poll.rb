0431cb7173bbf16cb25406612b86b4c85102811a
Marz Drel <marzdrel@users.noreply.github.com>
2025-05-24 15:10:55 +0200

    Annotate models

--- app/models/poll.rb
+++ app/models/poll.rb
@@ -1,3 +1,28 @@
+# rubocop:disable Layout/LineLength
+# == Schema Information
+# Schema version: 20250519211503
+#
+# Table name: polls
+#
+#  id          :integer          not null, primary key
+#  name        :string           not null
+#  description :string
+#  creator_id  :integer          not null
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#  eid         :uuid             not null
+#
+# Indexes
+#
+#  index_polls_on_creator_id  (creator_id)
+#  index_polls_on_eid         (eid) UNIQUE
+#
+# Foreign Keys
+#
+#  creator_id  (creator_id => users.id)
+#
+# rubocop:enable Layout/LineLength
+
 class Poll < ApplicationRecord
   include Exid::Record.new("poll", :eid)
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,25 @@
+# rubocop:disable Layout/LineLength
+# == Schema Information
+# Schema version: 20250519211503
+#
+# Table name: users
+#
+#  id                 :integer          not null, primary key
+#  created_at         :datetime         not null
+#  updated_at         :datetime         not null
+#  email              :string           not null
+#  encrypted_password :string(128)      not null
+#  confirmation_token :string(128)
+#  remember_token     :string(128)      not null
+#
+# Indexes
+#
+#  index_users_on_confirmation_token  (confirmation_token) UNIQUE
+#  index_users_on_email               (email)
+#  index_users_on_remember_token      (remember_token) UNIQUE
+#
+# rubocop:enable Layout/LineLength
+
 class User < ApplicationRecord
   include Clearance::User
 end
